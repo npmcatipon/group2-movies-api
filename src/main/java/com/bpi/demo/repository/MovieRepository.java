e0df0cb946fa582cb445920a5218a50685750d6e
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-05 10:55:41 +0800

    Revert "Zak movie"

--- src/main/java/com/bpi/demo/repository/MovieRepository.java
+++ src/main/java/com/bpi/demo/repository/MovieRepository.java
@@ -1,10 +1,5 @@
 package com.bpi.demo.repository;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bpi.demo.model.Movie;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long>{
+public class MovieRepository {
 
 }
--- src/main/java/com/bpi/demo/service/MovieService.java
+++ src/main/java/com/bpi/demo/service/MovieService.java
@@ -1,35 +1,5 @@
 package com.bpi.demo.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.bpi.demo.model.Movie;
-import com.bpi.demo.repository.MovieRepository;
-
-@Service
 public class MovieService {
 
-	private final MovieRepository movieRepo;
-	
-	public MovieService(MovieRepository movieRepo) {
-		this.movieRepo = movieRepo;
-	}
-	
-	//ADD MOVIE
-	public Movie addMovie(Movie movie) {
-		return movieRepo.save(movie);
-	}
-	
-	//GET MOVIE
-	public List<Movie> getAllMovies() {
-		return movieRepo.findAll();
-	}
-	
-	//UPDATE MOVIE
-	
-	//DELETE MOVIE
-	public void deleteMovie(Long id) {
-		movieRepo.deleteById(id);
-	}
 }
