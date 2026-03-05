771a53c88d631a3a09ecd58dfc4b956b52b6ad3f
John Isiah  A. Sanchez <jiasanchez@headoffice.corp.bpi.com.ph>
2026-03-05 11:09:01 +0800

    Added methods on Controller

--- src/main/java/com/bpi/demo/controller/MovieController.java
+++ src/main/java/com/bpi/demo/controller/MovieController.java
@@ -1,5 +1,43 @@
 package com.bpi.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.bpi.demo.model.*;
+
+import com.bpi.demo.service.MovieService;
+
+@RestController
+@RequestMapping("/api/movie")
 public class MovieController {
 
+	private final MovieService movieService;
+	
+	public MovieController(MovieService movieService) {
+		this.movieService = movieService;
+	}
+	
+	@GetMapping
+	public List<Movie> getMovies(@RequestParam(required = false) String movie){
+		return movieService.getAllMovies();
+	}
+	
+	@PostMapping
+	public Movie addMovie(@RequestBody Movie movie) {
+		return movieService.addMovie(movie);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteMovie(@PathVariable Long id) {
+		movieService.deleteMovie(id);
+		return "Movie Succesfully deleted";
+	}
+	
 }
