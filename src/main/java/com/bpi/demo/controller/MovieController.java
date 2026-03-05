62beac3722cf696be148497e085c0834439aceec
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-05 20:30:24 +0800

    added constructor on MovieController

--- src/main/java/com/bpi/demo/controller/MovieController.java
+++ src/main/java/com/bpi/demo/controller/MovieController.java
@@ -1,5 +1,19 @@
 package com.bpi.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bpi.demo.service.MovieService;
+
+@RestController
+@RequestMapping("/api/movies")
 public class MovieController {
 
+	private final MovieService movieService;
+	
+	public MovieController (MovieService movieService) {
+		this.movieService = movieService;
+	}
+	
+	
 }
