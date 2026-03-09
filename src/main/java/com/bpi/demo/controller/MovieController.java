d7ef68a2e47aa84a4480c54b319f59810ddf7170
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-09 10:51:44 +0800

    merged MovieController.java

--- src/main/java/com/bpi/demo/controller/MovieController.java
+++ src/main/java/com/bpi/demo/controller/MovieController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.bpi.demo.model.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bpi.demo.service.MovieService;
 
@@ -20,30 +22,29 @@ import com.bpi.demo.service.MovieService;
 public class MovieController {
 
 	private final MovieService movieService;
-	
+
 	public MovieController(MovieService movieService) {
 		this.movieService = movieService;
 	}
-	
+
 	@GetMapping
-	public List<Movie> getMovies(@RequestParam(required = false) String movie){
+	public List<Movie> getMovies(@RequestParam(required = false) String movie) {
 		return movieService.getAllMovies();
 	}
-	
+
 	@PostMapping
 	public Movie addMovie(@RequestBody Movie movie) {
 		return movieService.addMovie(movie);
 	}
-	
+
 	@PutMapping("/{id}")
-	public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie){
+	public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
 		return movieService.updateMovie(id, movie);
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public String deleteMovie(@PathVariable Long id) {
 		movieService.deleteMovie(id);
 		return "Movie Succesfully deleted";
 	}
-	
 }
